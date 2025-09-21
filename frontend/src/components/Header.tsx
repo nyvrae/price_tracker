53b8f726e6c898820bcce6b5ac167c45b13e2f18
nyvrae <mex99anue@gmail.com>
2025-09-21 16:28:59 +0300

    feat: renew Header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -5,35 +5,36 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        <header className="flex items-center justify-evenly px-32 py-8 bg-back text-text">
-            <div className="flex-shrink-0">
-                <a href="/" aria-label="Go to homepage">
-                    <Image
-                        src="/logo_cutted.png"
-                        alt="Price Tracker Logo"
-                        width={50}
-                        height={50}
-                    />
-                </a>
-            </div>
+        <header className="flex items-center justify-center pt-6">
+            <div className="flex items-center justify-evenly bg-white text-white w-[20svw] max-w-[500px] min-w-[300px] rounded-full px-4">
+                <div className="flex-shrink-0">
+                    <a href="/" aria-label="Go to homepage">
+                        <Image
+                            src="/logo2.png"
+                            alt="Price Tracker Logo"
+                            width={50}
+                            height={50}
+                        />
+                    </a>
+                </div>
 
-            <div className="flex-1 text-center">
-                <Link
-                    href={"/dashboard"}
-                    className="relative font-bebas tracking-widest text-2xl hover:tracking-wider transition-all duration-300 inline-block text-text group"
-                >
-                    Dashboard
-                    <span className="absolute left-0 -bottom-[2px] h-[2px] w-0 bg-text transition-all duration-300 group-hover:w-full"></span>
-                </Link>
-            </div>
+                <div className="flex-1 text-center">
+                    <Link
+                        href={"/dashboard"}
+                        className="relative font-bebas tracking-wider text-xl hover:text-accent transition-all duration-300 inline-block text-sub group"
+                    >
+                        Dashboard
+                    </Link>
+                </div>
 
-            <div className="flex items-center gap-4">
-                <button aria-label="View shopping cart" className="p-2 rounded hover:bg-white/10 transition-colors hover:cursor-pointer">
-                    <ShoppingCartOutlined className="text-3xl" />
-                </button>
-                <button aria-label="Open menu" className="p-2 rounded hover:bg-white/10 transition-colors hover:cursor-pointer">
-                    <MenuOutlined className="text-3xl" />
-                </button>
+                <div className="flex items-center gap-2">
+                    <button aria-label="View shopping cart" className="rounded hover:bg-sub/10 p-1">
+                        <ShoppingCartOutlined style={{ color: '#110b11', fontSize: '24px' }} />
+                    </button>
+                    <button aria-label="Open menu" className="rounded hover:bg-sub/10 p-1">
+                        <MenuOutlined style={{ color: '#110b11', fontSize: '24px' }} />
+                    </button>
+                </div>
             </div>
         </header>
     );
